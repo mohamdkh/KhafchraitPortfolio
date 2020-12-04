dd38262e9a4bd386c9b7dacc784bea5440bdf25a
mohamdkh <mohamedkhafchrait@gmail.com>
2020-12-04 17:03:35 +0100

    Auto-generated commit

--- src/app/utils/tags-data.ts
+++
@@ -1,8 +0,0 @@
-export const tagData = [
-  { displayName: 'FrontEnd', isSelected: true },
-  { displayName: 'Mobile', isSelected: true },
-  { displayName: 'JAVA', isSelected: true },
-  { displayName: '.NET', isSelected: true },
-  { displayName: 'SIG', isSelected: true },
-  { displayName: 'Autre', isSelected: !true }
-];
